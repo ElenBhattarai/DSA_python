d1d7d150ca598e00f11f5741c4c1a384aa5036d7
Elen Bhattarai <elen@Elens-Air.midcoip.net>
2022-05-31 21:38:09 -0500

    stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,5 +1,38 @@
 class Node:
     def __init__(self, data):
-    self.data = data
-    self.next = None
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    
+    #push
+    def push(self, value):
+        new_node = Node(value)
+        if self.top == None:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+    
+    #peek
+    def peek(self):
+        if self.top == None:
+            raise RuntimeError("Empty")
+        else:
+            return self.top.data
+    
+    #pop
+    def pop(self):
+        if self.top == None:
+            raise RuntimeError("Empty")
+        else:
+            self.top = self.top.next
+
+    #print all the values to check
+    def print_values(self):
+        while self.top is not None:
+            print(self.peek())
+            self.pop()
 
