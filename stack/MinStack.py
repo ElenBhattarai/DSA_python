37a2edd2139fa894f401f9b9c2360f3e2d7a9120
Elen Bhattarai <elen@Elens-Air.midcoip.net>
2022-06-12 18:22:46 -0500

    MinStack

---
+++ stack/MinStack.py
@@ -0,0 +1,28 @@
+from stack import Stack
+
+class MinStack:
+    def __init__(self):
+        self.mainStack = Stack()
+        self.helperStack = Stack()
+    
+    def push(self, val: int) -> None:
+        if self.mainStack.top is None:
+            self.mainStack.push(val)
+            self.helperStack.push(val)
+        else:
+            if val < self.helperStack.peek():
+                self.mainStack.push(val)
+                self.helperStack.push(val)
+            else:
+                self.mainStack.push(val)
+                self.helperStack.push(self.helperStack.peek())
+                
+    def pop(self) -> None:
+        self.helperStack.pop()
+        return self.mainStack.pop()
+    
+    def top(self) -> int:
+        return self.mainStack.peek()
+        
+    def getMin(self) -> int:
+        return self.helperStack.peek()
